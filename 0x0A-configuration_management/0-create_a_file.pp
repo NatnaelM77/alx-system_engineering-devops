bd1e1e28d604230f0b91c8677cc3ed49533ad89f
NantaelM77 <natnaelmehari77@gmail.com>
2022-03-12 06:47:53 -0800

    Update via upload

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file named school
+file {'school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
