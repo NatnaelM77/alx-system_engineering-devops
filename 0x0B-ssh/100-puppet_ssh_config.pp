2de3c7feeb85c635902eda3ec16f6af064c5215e
NantaelM77 <natnaelmehari77@gmail.com>
2022-04-04 14:06:13 -0700

    Fix puppet-lint error

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
 # connect to a server Execute a command
 
-file { 'ssh_config':
-  path => '/etc/ssh/ssh_config',
-  content => 'PasswordAuthentication no\nIdentityFile ~/.ssh/school',
+exec {'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+  path=>'/bin'
 }
