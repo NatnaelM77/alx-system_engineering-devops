de40091bfa25d4c1ae10153eaf7d8bcfc3926767
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-14 00:10:46 +0300

    Quick Fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""getting data from an api
+"""
 
-"""Returns information about his/her todos list progress."""
+import json
+from print import printf
+import requests
+from sys import argv
 
 if __name__ == '__main__':
-    import json
-    from print import printf
-    import requests
-    from sys import argv
-
     id = int(argv[1])
     todos = []
     req_user = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{id}')
+            f'https://jsonplaceholder.typicode.com/users/{id}')
     user = json.loads(req_user.text).get('name')
 
     req_todo = requests.get(f'https://jsonplaceholder.typicode.com/todos/')
