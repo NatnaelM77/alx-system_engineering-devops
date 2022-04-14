e4302129ffff121dde490bda2942b5cac2a32155
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-14 23:19:39 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-
 """
 Using a REST API, for a given employee ID and returns information
 about his/her TODO list progress.
 """
-
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
     todos = []
-    id = int(argv[1])
+    id = int(sys.argv[1])
     user = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{id}").json()
+        f'https://jsonplaceholder.typicode.com/users/{id}').json()
     req_todo = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos?userId={id}").json()
+        f'https://jsonplaceholder.typicode.com/todos?userId={id}').json()
     for todo in req_todo:
-        if todo.get("userId") == id and todo.get("completed"):
-            todos += [todo.get("title")]
-    print(f"Employee {user.get('name')} is done with tasks("
-          f"{len(todos)}/{len(req_todo)})):")
+        if todo.get('userId') == id and todo.get('completed'):
+            todos += [todo.get('title')]
+    print(f'Employee {user.get("name")} is done with tasks('
+          f'{len(todos)}/{len(req_todo)})):')
     for task in todos:
-        print("\t %s" % task)
+        print('\t %s' % task)
