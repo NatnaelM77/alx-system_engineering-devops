a50c08ee5327f8a04b28cbb5c801c0c15b377cea
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-14 22:49:30 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
 """
 Using a REST API, for a given employee ID and returns information
-about his/her to-do list progress.
+about his/her TODO list progress.
 """
-
 import requests
 import sys
-
-
-def printf(user_info, tasks):
-    print(f"Employee {user_info.get('name')} is done with tasks("
-          f"{user_info.get('task')}/20):")
-    if tasks:
-        for task in tasks:
-            print('\t %s' % task)
 
 
 if __name__ == '__main__':
     todos = []
     id = int(sys.argv[1])
     user = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{id}').json().get('name')
+        f'https://jsonplaceholder.typicode.com/users/{id}').json()
     req_todo = requests.get(
         f"https://jsonplaceholder.typicode.com/todos?userId={id}").json()
     for todo in req_todo:
         if todo.get('userId') == id and todo.get('completed'):
             todos += [todo.get('title')]
-    printf({'name': user, 'task': len(todos),
-                  'task_completed': len(req_todo)}, todos)
+    print(f"Employee {user.get('name')} is done with tasks("
+          f"{len(todos)}/{len(req_todo)})):")
+    for task in todos:
+        print('\t %s' % task)
