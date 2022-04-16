20590361f11285dc2405f6fc3a9726c22c7e29a9
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-16 21:16:00 +0300

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,16 +10,17 @@ import requests
 from sys import argv
 
 
-if __name__ == '__main__':
-    id = int(argv[1])
+if __name__ == "__main__":
+    user_id = int(argv[1])
+    base_url = "https://jsonplaceholder.typicode.com/"
     username = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{id}').json().get('username')
-    req_tasks = requests.get(
-        f'https://jsonplaceholder.typicode.com/todos?userId={id}').json()
-    with open(f"{id}.json", mode='w') as json_file:
-        task_dct = {id: []}
+        "{}users/{}".format(base_url, user_id)).json().get("username")
+    req_tasks = requests.get("{}todos".format(base_url), params={
+                             "userId": user_id}).json()
+    with open("{}.json".format(user_id), mode="w") as json_file:
+        task_dct = {user_id: []}
         for tasks in req_tasks:
-            task_dct[id] += [{'task': tasks['title'],
-                              'completed': tasks['completed'], 'username': username}]
+            task_dct[user_id] += [{"task": tasks["title"],
+                              "completed": tasks["completed"], "username": username}]
         data = json.dumps(task_dct)
         json_file.write(data)
