46e1bfdab2f625b87bdb0d86b9df6ade2dfa4743
NatnaelM77 <89731275+NatnaelM77@users.noreply.github.com>
2022-04-16 20:13:38 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-'''
+"""
 Returns information about his/her to list progress.
-'''
+"""
 import requests
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     todos = []
     id = int(sys.argv[1])
     user = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{id}').json()
+        f"https://jsonplaceholder.typicode.com/users/{id}").json()
     req_todo = requests.get(
-        f'https://jsonplaceholder.typicode.com/todos?userId={id}').json()
+        f"https://jsonplaceholder.typicode.com/todos?userId={id}").json()
     for todo in req_todo:
-        if todo.get('userId') == id and todo.get('completed'):
+        if todo.get("userId") == id and todo.get("completed"):
             todos += [todo.get('title')]
-    print(f'Employee {user.get("name")} is done with tasks('
-          f'{len(todos)}/{len(req_todo)})):')
+    print(f"Employee {user.get('name')} is done with tasks("
+          f"{len(todos)}/{len(req_todo)})):")
     for task in todos:
-        print('\t %s' % task)
+        print("\t %s" % task)
