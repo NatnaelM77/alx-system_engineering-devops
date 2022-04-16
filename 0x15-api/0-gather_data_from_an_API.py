2c21af36d27270da89a9c435f6302e333d386618
NatnaelM77 <89731275+NatnaelM77@users.noreply.github.com>
2022-04-16 20:18:55 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,18 +3,16 @@
 Returns information about his/her to list progress.
 """
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     todos = []
-    id = int(sys.argv[1])
-    user = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{id}").json()
-    req_todo = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos?userId={id}").json()
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(f"{base_url}/users{argv[1]}").json()
+    req_todo = requests.get(f"{base_url}/todos?userId={argv[1]}").json()
     for todo in req_todo:
         if todo.get("userId") == id and todo.get("completed"):
-            todos += [todo.get('title')]
+            todos += [todo.get("title")]
     print(f"Employee {user.get('name')} is done with tasks("
           f"{len(todos)}/{len(req_todo)})):")
     for task in todos:
