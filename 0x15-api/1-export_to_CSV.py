037732f1af25894b810964a0ab2e3ec916ce9516
NatnaelM77 <89731275+NatnaelM77@users.noreply.github.com>
2022-04-16 20:59:10 +0300

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 
-'''
+"""
 Exports to-do list information for a given employee
 ID to CSV format.
-'''
+"""
 
 import csv
 import requests
 from sys import argv
 
 
-if __name__ == '__main__':
-    id = int(argv[1])
-    username = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{id}').json().get('username')
-    req_tasks = requests.get(
-        f'https://jsonplaceholder.typicode.com/todos?userId={id}').json()
-    with open(f"{id}.csv", mode='w') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=',',
+if __name__ == "__main__":
+    todos = []
+    user_id = int(argv[1])
+    base_url = "https://jsonplaceholder.typicode.com/"
+    username = requests.get("{}users/{}".format(base_url, user_id)).json().get("username")
+    req_tasks = requests.get("{base_url}todos", params={"user_id": user_id}).json()
+    with open(f"{id}.csv", mode="w") as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=",",
                                 quotechar='"', quoting=csv.QUOTE_ALL)
         for row in req_tasks:
             csv_writer.writerow(
-                [row['userId'], username, row['completed'], row['title']])
+                [row["userId"], username, row["completed"], row["title"]])
