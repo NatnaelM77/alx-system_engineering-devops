6d926ed33eb253a6a937ffb304253ac2ad50076e
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-26 22:02:57 +0300

    Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -14,7 +14,8 @@ def recurse(subreddit, hot_list=[], after=None):
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     header = {'User-Agent': '0x16-api_advanced:2-recurse:v1.0.0 (by '
                             '/u/natnaelm77)'}
-    request = req.get(url, headers=header, allow_redirects=False)
+    request = req.get(url, headers=header, params={'after': after},
+                      allow_redirects=False)
     if request.status_code == 404:
         return None
     request = request.json().get('data')
