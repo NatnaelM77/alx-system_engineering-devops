d4b2b11ba3a778b6bcc9e9cfe4476ed5bd8d14d3
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-26 23:26:52 +0300

    Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ module that returns a list containing the titles of all hot articles
 import requests as req
 
 
-def recurse(subreddit, hot_list=[], after=None, count=0):
+def recurse(subreddit, hot_list=[], after=None):
     """returns a list containing the titles of all hot articles for a given
     subreddit.
     """
@@ -15,9 +15,7 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
     header = {'User-Agent': '0x16-api_advanced:2-recurse:v1.0.0 (by '
                             '/u/natnaelm77)'}
     params = {
-        'after': after,
-        'count': count,
-        'limit': 100
+        'after': after
     }
     request = req.get(url, headers=header, params=params,
                       allow_redirects=False)
@@ -25,10 +23,11 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
         return None
     request = request.json().get('data')
     after = request.get('after')
-    count += request.get("dist")
 
     hot_list.append(post.get('data').get('title') for post in request.get(
             'children'))
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    if after is None:
+        if len(hot_list) == 0:
+            return None
+        return hot_list
+    return recurse(subreddit, hot_list, after)
