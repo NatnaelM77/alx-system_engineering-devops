384d16e541b65db46dff24fcb9fa3c78e6243ee8
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-26 18:20:17 +0300

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,14 +12,11 @@ def top_ten(subreddit):
     subreddit.
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    header = {'User-Agent': '0x16-api_advanced:0-subs:v1.0.0 (by '
+    header = {'User-Agent': '0x16-api_advanced:1-top_ten:v1.0.0 (by '
                             '/u/natnaelm77)'}
-    if subreddit is None or type(subreddit) is not str:
+    request = req.get(url, headers=header).json().get('data')
+    if request.status_code == 404:
         print("None")
-    else:
-        request = req.get(url, headers=header).json().get('data')
-        if request.status_code == 404:
-            print("None")
-            return
-        for post in range(10):
-            print(request.get('children')[post].get('data').get('title'))
+        return
+    for post in range(10):
+        print(request.get('children')[post].get('data').get('title'))
