5d3684ca56c600056b665924cb2d53aa49035193
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-27 13:19:28 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# fix phpp extenstion to php
+# fix phpp extensions to php
 
 exec { 'fix-wp':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-setting.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
